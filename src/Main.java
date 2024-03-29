69f62c6a00c3a47e2673b178d9595ba18c75212e
Paul Franklin <paul.franklin@csuglobal.edu>
2024-03-29 17:03:50 -0400

    put lines in organized order; now more readable

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,37 @@ public class Main {
 
     public static void main(String[] args) {
         // Create a JPanel object
-       JFrame frame   = new JFrame("Bank Interface");
+        JFrame frame = new JFrame("Bank Interface");
         frame.setSize(1000, 700);
         frame.setLayout(new FlowLayout());
         JPanel balancePanel = new JPanel();
-        frame.add(balancePanel);
         JPanel depositPanel = new JPanel();
-        frame.add(depositPanel);
         JPanel withdrawalPanel = new JPanel();
-        frame.add(withdrawalPanel);
         JPanel currentBalancePanel = new JPanel();
-        frame.add(currentBalancePanel);
-        JTextField currentBalance = new JTextField("Current Balance");
-        balancePanel.add(currentBalance);
-        JTextField enterBalance = new JTextField("Enter Balance:");
-        balancePanel.add(enterBalance);
-        JTextField depositCash = new JTextField("Enter Deposit Amount:");
-        depositPanel.add(depositCash);
-        JTextField withdrawCash = new JTextField("Enter Withdrawal Amount:");
-        withdrawalPanel.add(withdrawCash);
-        JButton setBalanceButton = new JButton("Enter Balance");
-        balancePanel.add(setBalanceButton);
-        JButton depositButton = new JButton("Enter Deposit Amount");
-        depositPanel.add(depositButton);
-        JButton withdrawCashButton = new JButton("Enter Withdrawal Amount");
-        withdrawalPanel.add(withdrawCashButton);
-        frame.setVisible(true);
 
+        frame.add(balancePanel);
+        frame.add(depositPanel);
+        frame.add(withdrawalPanel);
+        frame.add(currentBalancePanel);
+
+        JTextField currentBalance = new JTextField("Current Balance");
+        JTextField enterBalance = new JTextField("Enter Balance:");
+        JTextField depositCash = new JTextField("Enter Deposit Amount:");
+        JTextField withdrawCash = new JTextField("Enter Withdrawal Amount:");
+
+        balancePanel.add(currentBalance);
+        balancePanel.add(enterBalance);
+        depositPanel.add(depositCash);
+        withdrawalPanel.add(withdrawCash);
+
+        JButton setBalanceButton = new JButton("Enter Balance");
+        JButton depositButton = new JButton("Enter Deposit Amount");
+        JButton withdrawCashButton = new JButton("Enter Withdrawal Amount");
+
+        balancePanel.add(setBalanceButton);
+        depositPanel.add(depositButton);
+        withdrawalPanel.add(withdrawCashButton);
+
+        frame.setVisible(true);
+        b.addActionListener(this);
     }}
